b1cacd36b418983ad29f7b7837c66cab4c8fd42d
Doug Ireton <dougireton@gmail.com>
2020-06-04 19:06:33 -0700

    Feat/fix tests (#2)
    * Unit tests work, create SlackMessage class
    
    * Add python byte-compiled files to .gitignore
    
    * Fix examples
    
    * Remove http file
    
    Co-authored-by: Doug Ireton <doug@1strategy.com>

---
+++ src/console_signin/console_signin.py
@@ -0,0 +1,66 @@
+import os
+import re
+
+import requests
+
+from aws_lambda_powertools.logging import Logger
+
+logger = Logger(service="Console Sign-in")
+
+class SlackMessage:
+    def __init__(self, user_arn):
+        self.user_arn = user_arn
+
+    def _user_name(self):
+        """
+        Extracts the username from an AWS User ARN
+
+        Parameters:
+            self.user_arn (string): AWS User ARN, e.g. arn:aws:iam::123456789012:user/doug@example.com
+
+        Returns:
+            username (string): Username in email format, e.g. doug@example.com
+        """
+        arn = re.search(r'arn:aws:iam::\d{12}:user/([a-z]+@\w+\.com)', self.user_arn)
+        return arn.group(1)
+
+    def _payload(self) -> dict:
+        p = {'username': self._user_name()}
+        return p
+        
+    def post(self, slack_url: str) -> requests.Response:
+        logger.debug(f"POSTing {self._payload()} to {slack_url}")
+
+        resp = requests.post(slack_url, json=self._payload())
+        logger.debug(f"POST to {slack_url} returned: {resp}")
+        return resp
+
+
+def lambda_handler(event, context):
+    """Sample Lambda function reacting to EventBridge events
+
+    Parameters
+    ----------
+    event: dict, required
+        Event Bridge Events Format
+
+        Event doc: https://docs.aws.amazon.com/eventbridge/latest/userguide/event-types.html
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+        The same input event file
+    """
+    master_acct_arn = event["detail"]["additionalEventData"]["SwitchFrom"]
+    sm = SlackMessage(master_acct_arn)
+    resp = sm.post(os.environ["slackWebhookURL"])
+
+    if resp.status_code != requests.codes.ok:
+        resp.raise_for_status()
+
+    logger.info(f"Posting to Slack channel returned {resp}.")
+    return resp.status_code
---
+++ tests/unit/test_console_signin.py
@@ -0,0 +1,73 @@
+import json
+import os
+import uuid
+import requests
+import requests_mock
+
+
+import pytest
+
+from console_signin.console_signin import SlackMessage
+
+
+class LambdaContext:
+    """Context class that mimics the AWS Lambda context
+    http://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    """
+    def __init__(self):
+        self.aws_request_id = uuid.uuid4().hex
+
+        self.log_group_name = '/aws/lambda/test'
+        self.log_stream_name = '2016-11-15blahblah'
+
+        self.function_name = 'test'
+        self.memory_limit_in_mb = '384'
+        self.function_version = '1'
+        self.invoked_function_arn = 'arn:aws:lambda:us-west-2:blahblah:function:test'
+
+        # FIXME(willkg): Keeping these as None until we need them.
+        self.client_context = None
+        self.identity = None
+
+EVENT_FILE = os.path.join(
+    os.path.dirname(__file__),
+    '..',
+    '..',
+    'events',
+    'console_sign_in.json'
+)
+
+# @pytest.fixture
+# def mock_env_slack_web_url(monkeypatch):
+#     monkeypatch.setenv("slackWebhookURL", "https://hooks.slack.com/workflows/foo/bar/")
+
+
+@pytest.fixture()
+def event(event_file=EVENT_FILE):
+    '''Trigger event'''
+    with open(event_file) as f:
+        return json.load(f)
+
+def test_slack_message_happy_path():
+    msg = SlackMessage("arn:aws:iam::123456789012:user/doug@example.com")
+
+    with requests_mock.Mocker() as m:
+        m.post("https://hooks.slack.com/workflows/valid/path")
+        resp = msg.post("https://hooks.slack.com/workflows/valid/path")
+
+    assert resp.status_code == 200
+
+def test_slack_message_invalid_url():
+    msg = SlackMessage("arn:aws:iam::123456789012:user/doug@example.com")
+    resp_body = {
+        "error": "invalid_token",
+        "ok": False,
+        "response_metadata": {
+            "messages": []
+        }
+    }
+    with requests_mock.Mocker() as m:
+        m.post("https://hooks.slack.com/workflows/invalid/token", json=resp_body, status_code=401)
+        resp = msg.post("https://hooks.slack.com/workflows/invalid/token")
+
+    assert resp.status_code == 401
